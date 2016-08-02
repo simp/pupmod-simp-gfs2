55afa387f1e721922f6abf89b0bccd6358346feb
Trevor Vaughan <tvaughan@onyxpoint.com>
2016-08-02 09:39:03 -0400

    (SIMP-1275) Update to new naming convention
    Ported to the new RPM naming scheme
    
    SIMP-1275 #comment ported to the new RPM naming scheme
    
    Change-Id: I91173d1938c7fa237dfbda16f64ca8aece0ddebe

--- spec/classes/cluster_allow_spec.rb
+++ spec/classes/cluster_allow_spec.rb
@@ -1,13 +1,13 @@
 require 'spec_helper'
 
 describe 'gfs2::cluster_allow' do
+  context 'supported operating systems' do
+    on_supported_os.each do |os, facts|
+      context "on #{os}" do
+        let(:facts) { facts }
 
-  base_facts = {
-    :operatingsystem => 'RedHat',
-    :lsbdistrelease  => '6'
-  }
-
-  let(:facts) {base_facts}
-
-  it { should create_class('gfs2::cluster_allow') }
+        it { should create_class('gfs2::cluster_allow') }
+      end
+    end
+  end
 end
